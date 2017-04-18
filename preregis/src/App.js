dcf78e35bca9ccf085b8aa2ed5034e30aefd465d
FM <ekkalakfm@gmail.com>
2017-04-18 23:12:02 +0700

    init web

---
+++ preregis/src/App.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div classNameName="App">
+        <nav className="navbar navbar-inverse">
+  <div className="container-fluid">
+    <div className="navbar-header">
+      <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </button>
+
+    </div>
+    <div className="collapse navbar-collapse" id="myNavbar">
+      <ul className="nav navbar-nav">
+      </ul>
+      <ul className="nav navbar-nav navbar-right">
+        <li><a href="#">Chulalongkorn University</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div className="container-fluid text-center">
+  <div className="row content">
+    <div className="col-sm-2 sidenav">
+    </div>
+    <div className="col-sm-8 text-left">
+        <div className="jumbotron text-center">
+              <h1>CU Pre-Regis</h1>
+              <hr/><hr/>
+
+              <div className="row">
+                  <div className="col-sm-2"></div>
+                  <div className="col-sm-1"><p>8</p></div>
+                  <div className="col-sm-1"><p>9</p></div>
+                  <div className="col-sm-1"><p>10</p></div>
+                  <div className="col-sm-1"><p>11</p></div>
+                  <div className="col-sm-1"><p>12</p></div>
+                  <div className="col-sm-1"><p>13</p></div>
+                  <div className="col-sm-1"><p>14</p></div>
+                  <div className="col-sm-1"><p>15</p></div>
+                  <div className="col-sm-1"><p>16</p></div>
+                  <div className="col-sm-1"><p></p></div>
+              </div>
+                  <div className="row">
+                      <div className="col-sm-2"><p>Monday</p></div>
+                      <div className="col-sm-2"><p id="rcorners1">Calculus I</p></div>
+
+                      <div className="col-sm-2"><p id="rcorners2">Gen Phy I</p></div>
+
+                      <div className="col-sm-1"><p> </p></div>
+                      <div className="col-sm-1"><p> </p></div>
+                      <div className="col-sm-1"><p> </p></div>
+                      <div className="col-sm-1"><p> </p></div>
+                      <div className="col-sm-1"><p> </p></div>
+                      <div className="col-sm-1"><p> </p></div>
+                    </div>
+                      <div className="row">
+                          <div className="col-sm-2"><p>Tuesday</p></div>
+
+                          <div className="col-sm-2"><p id="rcorners1">Comp Prog</p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                      </div>
+                      <div className="row">
+                          <div className="col-sm-2"><p>Wednesday</p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                      </div>
+                      <div className="row">
+                          <div className="col-sm-2"><p>Thursday</p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                      </div>
+                      <div className="row">
+                          <div className="col-sm-2"><p>Friday</p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                          <div className="col-sm-1"><p> </p></div>
+                      </div>
+        </div>
+    </div>
+    <div className="col-sm-2 sidenav">
+
+    </div>
+  </div>
+</div>
+
+<footer className="container-fluid text-center">
+  <p>Footer Text</p>
+</footer>
+      </div>
+    );
+  }
+}
+
+export default App;
