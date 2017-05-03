2eca6b71e936e82aea86b90160e996f733c6233f
FM <ekkalakfm@gmail.com>
2017-05-04 02:48:58 +0700

    edit class to className

--- preregis/src/container/search_panel.js
+++ preregis/src/container/search_panel.js
@@ -9,10 +9,10 @@ class SearchPanel extends Component {
                 <hr/>
                 <div className="container-fluid">
                     <div className="col-sm-3" />
-                    <input type="text" class="form-control" className="col-sm-6 search searchTime" placeholder="Time : 9.00" aria-describedby="basic-addon1" />
+                    <input type="text" className="form-control" className="col-sm-6 search searchTime" placeholder="Time : 9.00" aria-describedby="basic-addon1" />
                     <div className="col-sm-10" />
-                    <input type="text" class="form-control" className="col-sm-10 search searchNo" placeholder="Course NO. : 88888888" aria-describedby="basic-addon1" />
-                    <input type="text" class="form-control" className="col-sm-12 search searchName" placeholder="Course Name : Gen Phy II" aria-describedby="basic-addon1" />
+                    <input type="text" className="form-control" className="col-sm-10 search searchNo" placeholder="Course NO. : 88888888" aria-describedby="basic-addon1" />
+                    <input type="text" className="form-control" className="col-sm-12 search searchName" placeholder="Course Name : Gen Phy II" aria-describedby="basic-addon1" />
                     <br/>
                 </div>
                 <button className="searchbotton">search</button>
