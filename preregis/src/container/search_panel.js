564f080fbcce6bcf8792a237fdf0eff33671cad9
focusjk <jiraphat.khupanit@gmail.com>
2017-05-05 01:20:11 +0700

    new search bar

--- preregis/src/container/search_panel.js
+++ preregis/src/container/search_panel.js
@@ -8,22 +8,25 @@ class SearchPanel extends Component {
                     <h3>List of course</h3>
                     <hr/>
 
-                    <div className="col-sm-3" />
                     <input type="text" className="form-control" className="col-sm-6 search searchTime" placeholder="Time : 9.00" aria-describedby="basic-addon1" />
                     <div className="col-sm-12" />
-                    <div className="col-sm-2" />
                     <input type="text" className="form-control" className="col-sm-8 search searchNo" placeholder="Course NO. : 88888888" aria-describedby="basic-addon1" />
-                    <br/>
                     <input type="text" className="form-control" className="col-sm-12 search searchName" placeholder="Course Name : Gen Phy II" aria-describedby="basic-addon1" />
+
+                    
+                    <form>
+                        <div className="input-group">
+                            <span className="input-group-addon"><i className="glyphicon glyphicon-user"></i></span>
+                            <input id="email" type="text" className="form-control" name="email" placeholder="Email" />
+                        </div>
+                    </form>
                 
                 <br/>
                 <button className="btn btn-primary" type="botton">search</button>
                 <hr/>
 
                 <div className="col-sm-12 searched-course-name">
-                    88888888 : Fmmmm 
-                    <div className="searched-credit">(2 credit)</div>
-                    <br/>   
+                    88888888 : Fmmmm<div className="searched-credit">(2 credit)</div>
                 </div>
                 
 
@@ -34,6 +37,18 @@ class SearchPanel extends Component {
                 </botton>
 
                 <botton className="courselist2 col-sm-6">
+                    By aj.Fmmmm
+                    <br/>
+                    Time : 9.00-11.00
+                </botton>
+
+                <botton className="courselist col-sm-6">
+                    By aj.Fmmmm
+                    <br/>
+                    Time : 9.00-11.00
+                </botton>
+
+                <botton className="courselist col-sm-6">
                     By aj.Fmmmm
                     <br/>
                     Time : 9.00-11.00
