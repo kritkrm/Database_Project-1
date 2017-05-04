dd2e03c066893e63f6cc359d79b22b9594c66d02
focusjk <jiraphat.khupanit@gmail.com>
2017-05-04 18:48:15 +0700

    start making course list

--- preregis/src/container/search_panel.js
+++ preregis/src/container/search_panel.js
@@ -5,18 +5,47 @@ class SearchPanel extends Component {
     return (
         <div className="col-sm-4 text-center">
             <div className="jumbotron">
-                <h3>List of course</h3>
-                <hr/>
-                <div className="container-fluid">
+                    <h3>List of course</h3>
+                    <hr/>
+
                     <div className="col-sm-3" />
                     <input type="text" className="form-control" className="col-sm-6 search searchTime" placeholder="Time : 9.00" aria-describedby="basic-addon1" />
-                    <div className="col-sm-10" />
-                    <input type="text" className="form-control" className="col-sm-10 search searchNo" placeholder="Course NO. : 88888888" aria-describedby="basic-addon1" />
-                    <input type="text" className="form-control" className="col-sm-12 search searchName" placeholder="Course Name : Gen Phy II" aria-describedby="basic-addon1" />
+                    <div className="col-sm-12" />
+                    <div className="col-sm-2" />
+                    <input type="text" className="form-control" className="col-sm-8 search searchNo" placeholder="Course NO. : 88888888" aria-describedby="basic-addon1" />
                     <br/>
-                </div>
+                    <input type="text" className="form-control" className="col-sm-12 search searchName" placeholder="Course Name : Gen Phy II" aria-describedby="basic-addon1" />
+                
+                <br/>
                 <button className="searchbotton">search</button>
                 <hr/>
+
+                <botton className="courselist col-sm-6">
+                    88888888 (2 credit)
+                    <br/>
+                    Fmmmm
+                    <br/>
+                    By aj.Fmmmm
+                    <br/>
+                    Time : 9.00-11.00
+                </botton>
+
+                <botton className="courselist2 col-sm-6">
+                    88888888 (2 credit)
+                    <br/>
+                    Fmmmm
+                    <br/>
+                    By aj.Fmmmm
+                    <br/>
+                    Time : 9.00-11.00
+                </botton>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
             </div>
         </div>
     );
@@ -24,3 +53,4 @@ class SearchPanel extends Component {
 }
 
 export default SearchPanel;
+
