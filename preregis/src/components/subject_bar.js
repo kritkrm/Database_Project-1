838913ed8ef5882e1fe5ff9b76a850fe6870ce78
FM <ekkalakfm@gmail.com>
2017-05-05 02:45:25 +0700

    add state for table

--- preregis/src/components/subject_bar.js
+++ preregis/src/components/subject_bar.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 class SubjectBar extends Component {
     render() {
+            if(this.props.period == 0) {
+                return (
+                    <div className="col-sm-1"><p> </p></div>
+                );
+            }
             let per = "col-sm-" + this.props.period.toString();
             return (
                  <div className={per}><p id="rcorners1">{this.props.name}</p></div>
--- preregis/src/container/table.js
+++ preregis/src/container/table.js
@@ -2,85 +2,88 @@ import React, { Component } from 'react';
 import SubjectBar from '../components/subject_bar';
 
 class Table extends Component {
-  render() {
-    return (
-        <div className="col-sm-8 text-left">
-            <div className="jumbotron">
-                  <h1>CU Pre-regis</h1>
-                  <div className="row" >
-                      <div className="col-sm-2"></div>
-                      <div className="col-sm-1"><p>8</p></div>
-                      <div className="col-sm-1"><p>9</p></div>
-                      <div className="col-sm-1"><p>10</p></div>
-                      <div className="col-sm-1"><p>11</p></div>
-                      <div className="col-sm-1"><p>12</p></div>
-                      <div className="col-sm-1"><p>13</p></div>
-                      <div className="col-sm-1"><p>14</p></div>
-                      <div className="col-sm-1"><p>15</p></div>
-                      <div className="col-sm-1"><p>16</p></div>
-                      <div className="col-sm-1"><p></p></div>
-                  </div>
-                  <hr/>
-                  <div className="row">
-                      <div className="col-sm-2 text-center"><p>Monday</p></div>
-                      <div className="col-sm-2"><p id="rcorners1">Calculus I</p></div>
-                      <div className="col-sm-2"><p id="rcorners1">Gen Phy I</p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                  </div>
-                  <div className="row">
-                      <div className="col-sm-2 text-center"><p>Tuesday</p></div>
-                      <SubjectBar period={2} name="Calculus" />
-                      <SubjectBar period={2} name="Focus" />
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                  </div>
 
-                  <div className="row">
-                      <div className="col-sm-2 text-center"><p>Wednesday</p></div>
-                      <SubjectBar period={2} name="FMM" />
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                  </div>
+    constructor(props) {
+        super(props);
 
-                  <div className="row">
-                      <div className="col-sm-2 text-center"><p>Thursday</p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                  </div>
+        this.state = {
+                subjects: [
+                    [
+                        {name: "Focus", period: 2}, {name: "", period: 0},
+                        {name: "", period: 0}, {name: "", period: 0}, {name: "", period: 0},
+                        {name: "", period: 0}, {name: "", period: 0}
+                    ],
+                    [
+                        {name: "", period: 0}, {name: "FMM", period: 2}, {name: "", period: 0},
+                        {name: "", period: 0}, {name: "", period: 0}, {name: "", period: 0},
+                        {name: "", period: 0}
+                    ],
+                    [
+                        {name: "", period: 0}, {name: "", period: 0}, {name: "", period: 0},
+                        {name: "", period: 0}, {name: "", period: 0}, {name: "", period: 0},
+                        {name: "", period: 0}, {name: "", period: 0}
+                    ],
+                    [
+                        {name: "", period: 0}, {name: "", period: 0}, {name: "", period: 0},
+                        {name: "KUY", period: 4}, {name: "", period: 0}, {name: "", period: 0}
+                    ],
+                    [
+                        {name: "", period: 0}, {name: "", period: 0}, {name: "", period: 0},
+                        {name: "", period: 0}, {name: "", period: 0}, {name: "", period: 0},
+                        {name: "", period: 0}, {name: "", period: 0}
+                    ],
+                ]
+        };
+    }
 
-                  <div className="row">
-                      <div className="col-sm-2 text-center"><p>Friday</p></div>
-                      <SubjectBar period={5} name="Focus" />
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                      <div className="col-sm-1"><p> </p></div>
-                  </div>
+    renderSubjects(subject) {
+        return (
+            <SubjectBar period={subject.period} name={subject.name} />
+        );
+    }
+
+    renderTimePeriods(timePeriod) {
+        return (
+            <div className="col-sm-1"><p>{timePeriod}</p></div>
+        );
+    }
+
+    render() {
+        let time_periods = ['8', '9', '10', '11', '12', '13', '14', '15', '16'];
+        return (
+            <div className="col-sm-8 text-left">
+                <div className="jumbotron">
+                      <h1>CU Pre-regis</h1>
+                      <div className="row" >
+                          <div className="col-sm-2"></div>
+                          {time_periods.map(this.renderTimePeriods)}
+                      </div>
+                      <hr/>
+                      <div className="row">
+                          <div className="col-sm-2 text-center"><p>Monday</p></div>
+                          {this.state.subjects[0].map(this.renderSubjects)}
+                      </div>
+                      <div className="row">
+                          <div className="col-sm-2 text-center"><p>Tuesday</p></div>
+                          {this.state.subjects[1].map(this.renderSubjects)}
+                      </div>
+
+                      <div className="row">
+                          <div className="col-sm-2 text-center"><p>Wednesday</p></div>
+                          {this.state.subjects[2].map(this.renderSubjects)}
+                      </div>
+
+                      <div className="row">
+                          <div className="col-sm-2 text-center"><p>Thursday</p></div>
+                          {this.state.subjects[3].map(this.renderSubjects)}
+                      </div>
+
+                      <div className="row">
+                          <div className="col-sm-2 text-center"><p>Friday</p></div>
+                          {this.state.subjects[4].map(this.renderSubjects)}
+                      </div>
+                </div>
             </div>
-        </div>
     );
   }
 }
