cab3014b8750882b55a492ec70bad11b8805f78c
FM <ekkalakfm@gmail.com>
2017-05-06 02:10:21 +0700

    add logo

--- preregis/src/container/nav_bar.js
+++ preregis/src/container/nav_bar.js
@@ -4,8 +4,8 @@ class NavBar extends Component {
   render() {
     return (
         <div>
-            <nav className="navbar navbar-inverse">
-                <div className="container-fluid">
+
+            <nav className="navbar navbar-inverse" role="navigation">
 
                     <div className="navbar-header">
                         <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -13,20 +13,18 @@ class NavBar extends Component {
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
                         </button>
+                    <img className="navbar-brand sm-padding" src="../../resource/logo2.png" />
                     </div>
 
                     <div className="collapse navbar-collapse" id="myNavbar">
-                        <ul className="nav navbar-nav">
-                        </ul>
                         <ul className="nav navbar-nav navbar-left">
                             <li><p>Chulalongkorn University</p></li>
                         </ul>
+
                         <ul className="nav navbar-nav navbar-right">
                             <li><a href="#">FM & FOCUS KUY ▼</a></li>
                         </ul>
                     </div>
-
-                </div>
             </nav>
 
         </div>
