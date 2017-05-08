9c678eb12b414eeb897b1d9d24fe4d541c9b85b9
focusjk <jiraphat.khupanit@gmail.com>
2017-05-09 04:29:12 +0700

    put modal

--- preregis/src/container/table.js
+++ preregis/src/container/table.js
@@ -54,10 +54,45 @@ class Table extends Component {
     render() {
         let time_periods = ['8', '9', '10', '11', '12', '13', '14', '15', '16' ,'17'];
         return (
-        
-            <div className="jumbotron">
 
-            <div className="col-sm-8 text-left  my-table" >
+              <div className="outterjumbotron">
+                <div className="all-icon">
+                  <a href="#"><i className="my-icon glyphicon glyphicon-list"></i></a>
+                  
+                      
+                      <button type="button" className="my-icon glyphicon glyphicon-picture" data-toggle="modal" data-target="#myModal"></button>
+                      
+                      <div id="myModal" className="modal fade" role="dialog">
+                        <div className="modal-dialog modal-lg">
+                          <div className="modal-content">
+                            
+                            <div className="modal-header">
+                              <button type="button" className="close" data-dismiss="modal">&times;</button>
+                              <h4 className="modal-title text-center">CU Pre regis</h4>
+                            </div>
+                            
+                            <div className="modal-body">
+                              <p>Some text in the modal.</p>
+                            </div>
+      
+                            <div className="modal-footer">
+                              <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                            </div>
+
+                          </div>
+                        </div>
+                      </div>
+
+
+
+
+
+
+
+
+                </div>
+                <div className="jumbotron ">
+                  <div className="col-sm-8 text-left my-table" >
                 
                       <div className="row" >
                           <div className="col-xs-2 blackline blankbox"></div>
@@ -88,9 +123,11 @@ class Table extends Component {
                           <div className="col-xs-2 text-center"><p>Friday</p></div>
                           {this.state.subjects[4].map(this.renderSubjects)}
                       </div>
+                  </div>          
+                </div>
 
-            </div>          
-            </div>
+
+              </div>
     );
   }
 }
