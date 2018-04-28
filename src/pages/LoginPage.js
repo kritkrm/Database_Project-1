95cf1e87428adab479c9dc4b431e93f0fbded39f
Chanokthorn Uerpairojkit <chankychanky@hotmail.com>
2018-04-28 17:09:04 +0700

    loginComplete

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -40,6 +40,8 @@ class LoginPage extends Component {
 
     handleSubmit = (event) => {
 
+        let that = this;
+
         console.log( this.state.username ) ;
         if( this.state.type == "instructor" ) {
             console.log("inst sub");
@@ -52,10 +54,10 @@ class LoginPage extends Component {
                 } else { 
                     console.log( response.data );
                     console.log('LOGIN INST SUCCESS');
-                    this.cookies.set('token', response.data.token);
-                    this.props.changePage('teacherDashboard')
-                    this.props.changeID(response.data.instructor_id);
-                    this.props.changeType("instructor");
+                    that.cookies.set('token', response.data.token);
+                    that.props.changePage('teacherDashboard')
+                    that.props.changeID(response.data.instructor_id);
+                    that.props.changeType("instructor");
                 }
             }.bind(this)).catch(function (err) {
                  console.error(err);
@@ -71,10 +73,11 @@ class LoginPage extends Component {
                 } else { 
                     console.log( response.data );
                     console.log('LOGIN STU SUCCESS');
-                    this.props.changePage('studentDashboard')
-                    this.props.changeID(response.data.student_id);
-                    this.props.changeType("student");
-                    this.cookies.set('token', response.data.token);
+                    that.cookies.set('token', response.data.token);
+                    that.props.changePage('studentDashboard')
+                    that.props.changeID(response.data.student_id);
+                    that.props.changeType("student");
+                    
                 }
                 // console.log( response );
                 // console.log('LOGIN INST SUCCESS');
